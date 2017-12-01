9a8829a536a179a7a2e01f1cdf435abc3888343e
mocakry <31640677+mocakry@users.noreply.github.com>
2017-12-01 15:13:43 +0800

    xxx

--- src/Redux/Reducer/index.js
+++
@@ -1,9 +0,0 @@
-const getLunboImgUrl = (state=[],info)=>{
-    let {type,payload} = info ;
-    switch(type){
-        case 'lunboImgUrl' : return payload;
-        default:return state ;
-    }
-}
-
-export default getLunboImgUrl;
--- src/Redux/Store/index.js
+++
@@ -1,8 +0,0 @@
-import {createStore,applyMiddleware} from "redux";
-import reduxpromise from "redux-promise";
-
-import getLunboImgUrl from "../Reducer";
-
-const store = createStore(getLunboImgUrl,applyMiddleware(reduxpromise));
-
-export default store;
